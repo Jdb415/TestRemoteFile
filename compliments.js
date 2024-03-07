ccb9467e3810c4916c89e72d3f7a00fab48b25f1
Jdb415 <jasondebarros@gmail.com>
2024-03-06 20:07:00 -0800

    Create compliments.js

---
+++ compliments.js
@@ -0,0 +1,39 @@
+{
+    "anytime": [ 
+        "Music > News",
+        "Paper > Screens",
+        "Be nice to your parents",
+        "Amor Fati",
+        "Memento Mori",
+        "Spread good vibes",        
+        "Everything in life is temporary",
+        "Create > Consume",
+        "Play some music",  
+        "Do work", 
+        "Better everyday", 
+        "Tell your parents you love them",
+        "Stop procrastinating!", 
+        "Text a friend today", 
+        "Spotify > YouTube" 
+    ],
+    "morning": [
+        "Take a walk",
+        "Make your bed, make your life",
+        "New day, new start",
+        "Do your Duo!"          
+    ],
+    "afternoon": [ 
+
+     ],
+    "evening": [    
+        "Sleep is a superpower",
+        "Nightly routines are OP",
+        "Wear your retainer!"
+    ],
+    "rain": [
+        "Dress for rain today"
+    ],
+    "showers": [
+        "Dress for rain today"
+    ]
+}
